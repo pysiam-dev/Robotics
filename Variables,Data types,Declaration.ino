f70f94039f4dcdd26040733bc0e015914f6b3baa
Siam Al Hossen <147187634+pysiam-dev@users.noreply.github.com>
2026-01-14 01:01:15 +0600

    Add files via upload

---
+++ Variables,Data types,Declaration.ino
@@ -0,0 +1,25 @@
+// Variable Types:
+// 1. Integer 1,2,3,4,5,100,1000
+// 2. Float 1.2,3.4,12.3
+// 3. Char 'a','b','c','d','x','y','z'
+// 4. String "red","ball","green","car","hello world"
+
+
+void setup()
+{
+  Serial.begin(9600);
+  //Variable Declaration : data_type Variable_name:
+  int robot=2;
+  float car;
+  car=3.3454;
+  String name;
+  name="Siam";
+  
+  Serial.println(robot);
+  
+}
+
+void loop()
+{
+  
+}
