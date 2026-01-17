7451f11730795a036a148dfbaa5569a0ef031fbd
Siam Al Hossen <147187634+pysiam-dev@users.noreply.github.com>
2026-01-17 17:10:33 +0600

    Add files via upload

---
+++ Global Variable.ino
@@ -0,0 +1,16 @@
+// C++ code
+//
+
+int Setup=2;
+
+void setup()
+{
+  Serial.begin(9600);
+  
+  
+}
+
+void loop()
+{
+  Serial.println(Setup);
+}
---
+++ Local Variable.ino
@@ -0,0 +1,13 @@
+// C++ code
+//
+void setup()
+{
+  Serial.begin(9600);
+  int setup=2;
+  Serial.println(setup);
+}
+
+void loop()
+{
+  Serial.println(setup);
+}
